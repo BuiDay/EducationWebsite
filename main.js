e7ad10c4f64d61ca8c0f34aa19ce40c3604df494
BuiDay <buivanduynhat@gmail.com>
2022-07-13 02:09:58 +0700

    five

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const nav = document.querySelector("nav");
 const faqs = document.querySelectorAll(".faq");
+const navMenu = document.querySelector(".nav__menu");
+const btnOpenMenu = document.querySelector("#open-menu-btn");
+const btnCloseMenu = document.querySelector("#close-menu-btn");
+const teams = document.querySelectorAll(".team");
 
 window.addEventListener("scroll", () => {
   nav.classList.toggle("window-srcoll", window.scrollY > 50);
@@ -27,3 +31,41 @@ function removeOpenFaq() {
     faq.classList.remove("open");
   });
 }
+
+// window.addEventListener('resize', function(event){
+//   var newWidth = window.innerWidth;
+//   if(newWidth < 1023){
+//     navMenu.style.display = "flex";
+//     btnCloseMenu.style.display = "none";
+//     btnOpenMenu.style.display = "none";
+//     console.log(newWidth)
+//   }
+// });
+
+  btnOpenMenu.addEventListener("click",()=>{
+    navMenu.style.display = "flex";
+    btnCloseMenu.style.display = "inline-block";
+    btnOpenMenu.style.display = "none";
+  });
+  
+  btnCloseMenu.addEventListener("click",()=>{
+    navMenu.style.display = "none";
+    btnCloseMenu.style.display = "none";
+    btnOpenMenu.style.display = "inline-block";
+  });
+
+// teams.forEach((team)=>{
+//   team.addEventListener("mouseover",()=>{
+//     const team__social = team.querySelector(".team__social");
+//     team__social.style.opacity = "1";
+//   });
+// });
+
+//   teams.forEach((team)=>{
+//     team.addEventListener("mouseout",()=>{
+//       const team__social = team.querySelector(".team__social");
+//       team__social.style.opacity = "0";
+//     });
+
+// });
+
